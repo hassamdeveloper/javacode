cf4c5ed5b40505fdc1ef8914489185760e044682
Hassam jutt <hassamahmad310@gmail.com>
2023-04-29 17:35:28 +0500

    java_addition_code

---
+++ jawapractice/src/main/java/com/example/jawapractice/MyClass.java
@@ -0,0 +1,14 @@
+package com.example.jawapractice;
+
+public class MyClass {
+    public static void main(String[] args) {
+//        System.out.println("Hassam_Jutt");
+        int a=10;
+        int b=20;
+        int c=b*a;
+        System.out.println("sum is ="+ c);
+
+
+    }
+
+}
